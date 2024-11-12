743ec594189fc52d3b1f9dc50d1d4a5a0961c867
spark3557 <90881091+spark3557@users.noreply.github.com>
2024-11-12 12:08:07 -0500

    Update data.sql

--- data.sql
+++ data.sql
@@ -20,39 +20,40 @@ SET time_zone = "+00:00";
 -- Database: `php_docker`
 -- --------------------------------------------------------
 
+
 CREATE TABLE `Account` (
   `acc_id` int NOT NULL,
-  `task_id` int not null,
-  `email_add`varchar(255) not null,
+  `email_add` varchar(255) NOT NULL,
   `full_name` varchar(255) NOT NULL,
   `job_position` text NOT NULL,
   `date_started` date NOT NULL,
-  `salary` int not null,
-  `department` varchar(255) not null,
-  `acc_password` varchar(255) not null,
-  foreign key (Tasks) references task_id) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `salary` int NOT NULL,
+  `department` varchar(255) NOT NULL,
+  `acc_password` varchar(255) NOT NULL,
+  PRIMARY KEY (`acc_id`)
+);
 
-INSERT INTO `Account` (`acc_id`,`task_id`,`email_add`, `full_name`, `job_position`, `date_started`,`salary`,`department`,`acc_password`) VALUES
-(1, 1,'spark20@umbc.edu','Savannah Park', 'Program manager', '2022-09-01',70000,'Finance','blue12!'),
-(2, 2, 'moonstar@gmail.com','Luna Moon', 'Project Scheduler', '2022-09-03',65000,'Engineering', 'engine20@'),
-(3, 3, 'devdev12@gmail.com','Deven Roe', 'Financial Analyst', '2022-09-07',70000,'Finance','icemoney100!'),
-(4, 4, 'randomstuff12@gmail.com','Bob Rodger', 'Receptionist', '2022-09-07',60000,'Finance', 'blahblah09@');
+INSERT INTO `Account` (`acc_id`,`email_add`, `full_name`, `job_position`, `date_started`,`salary`,`department`,`acc_password`) VALUES
+(1, 'spark20@umbc.edu','Savannah Park', 'Program manager', '2022-09-01',70000,'Finance','blue12!'),
+(2, 'moonstar@gmail.com','Luna Moon', 'Project Scheduler', '2022-09-03',65000,'Engineering', 'engine20@'),
+(3, 'devdev12@gmail.com','Deven Roe', 'Financial Analyst', '2022-09-07',70000,'Finance','icemoney100!'),
+(4, 'randomstuff12@gmail.com','Bob Rodger', 'Receptionist', '2022-09-07',60000,'Finance', 'blahblah09@');
 
 CREATE TABLE `Tasks` (
   `task_id` int NOT NULL,
-  `acc_id` int not null,
+  `acc_id` int NOT NULL,
   `task_name` varchar(255) NOT NULL,
   `task_percentage` int NOT NULL,
   `task_deadline` date NOT NULL,
-  foreign key (Account) references acc_id)
-  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`task_id`),
+  FOREIGN KEY (`acc_id`) REFERENCES `Account`(`acc_id`)
+);
 
 INSERT INTO `Tasks` (`task_id`,`acc_id`, `task_name`, `task_percentage`, `task_deadline`) VALUES
 (1, 1,'Monthly Reports', 50, '2025-02-01'),
 (2, 2,'Baseline Execution Index', 65, '2024-05-02'),
 (3, 3,'WBS Creation', 90, '2025-09-07'),
 (4, 4,'Host Media Forum', 75, '2025-04-07');
-
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
